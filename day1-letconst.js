b518fc3f8f3fe27b3b8628b8127cfb2b820d783b
Sumeet Balwade <balwadesr@gmail.com>
2022-02-17 20:04:16 +0530

    day 1

---
+++ day1-letconst.js
@@ -0,0 +1,35 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let r = parseFloat(readLine());
+    const PI = Math.PI;
+    
+    // Print the area of the circle:
+    console.log(PI * r * r);
+
+    // Print the perimeter of the circle:
+    console.log(PI * 2 * r);
+
+    try {    
